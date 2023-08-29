07c4fc8c5aa6d57b27bd219f3b90a102a30c1f8c
sushramu <sushramu@gmail.com>
2023-08-29 06:37:29 +0530

    firstpush

---
+++ src/test/java/test.java
@@ -0,0 +1,19 @@
+import org.testng.annotations.Test;
+
+public class test {
+	@Test
+	public void test()
+	{
+
+		int AddedbySecond1;
+
+		int AddedbyFirst;
+
+		System.out.println("Test");
+		System.out.println("From Second engineer");
+		System.out.print("From first system engineer");
+		int AddedbySecond2;
+
+	}
+//adding comment
+}
